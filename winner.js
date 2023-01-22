3a68b5c1cf2a33493e285c67ed47e22dab8703d3
casmirchuks_ <50203957+casmirchuks@users.noreply.github.com>
2023-01-22 19:34:05 +0200

    Add files via upload

--- winner.js
+++ winner.js
@@ -3,34 +3,24 @@ const fs = require('fs');
 fs.readFile('input.txt', 'utf8', function (error, data) {
     if (error) throw error;
 
+    try {
     let players = {};
-    let showScore = " ";
     // split the file contents by new line
     const lines = data.split('\n');
-
     for(let line of lines){
         let [name, cards] = line.split(':');
         let cardList = cards;
-        try {
-            cardList = cards.split(',').map(card => card.trim()); 
-        } catch (error) {
-            showScore = `Exception: ${error}`
-        }
-
+        cardList = cards.split(',').map(card => card.trim()); 
         let score = 0;
         let suitScore = 1;
-
-        console.log(cardList);
         for (let card of cardList){
             let value = card[0];
             let suit = card[1];
-
             if(card.length === 3){
                 value = card[0] + card[1];
             } else {
                 value = card[0];
             }
-
             switch (value){
                 case "J":
                 case "j": 
@@ -72,7 +62,6 @@ fs.readFile('input.txt', 'utf8', function (error, data) {
                     suitScore += parseInt(suit)
             }
         }
-
         players[name] = {score: score, suitScore: suitScore};
     }
     console.log(players); 
@@ -89,13 +78,12 @@ fs.readFile('input.txt', 'utf8', function (error, data) {
             maxScorePlayer = [player];
         } else if (players[player].score === maxScore) {
             maxScorePlayer.push(player)
-            let showTieData = `${maxScorePlayer.join(", ")} : ${maxScore}`
-            console.log(`It is a tie between ${showTieData}, same face value score`);
+            // let showTieData = `${maxScorePlayer.join(",")} : ${maxScore}`
+            // console.log(`It is a tie between ${showTieData}, same face value score`);
         }
-
+    }
         // invokes if there is a tie (more 2 players with the same maxScore)
         if(maxScorePlayer.length > 1){
-
             for (let player of maxScorePlayer){
                 if (players[player].suitScore > maxSuitScore){
                     maxSuitScore = players[player].suitScore;
@@ -104,30 +92,17 @@ fs.readFile('input.txt', 'utf8', function (error, data) {
                     maxSuitScorePlayers.push(player)
                 }
             }
-            // console.log(maxSuitScorePlayers);
-            let showSuitScore = `${maxSuitScorePlayers.join(", ")}: ${maxSuitScore}`
-            console.log(showSuitScore);
-        } 
-    }
-     
-    // Data written to  file.
+            // let showSuitScore = `maxSuitScorePlayer: ${maxSuitScorePlayers.join(", ")}:${maxSuitScore}`
+            // console.log(showSuitScore);
 
-    if (maxSuitScorePlayers.length === 2){
-        showScore = `${maxSuitScorePlayers.join(",")}:${maxScore}`;
-        console.log( showScore);
-    } else if (maxSuitScorePlayers.length === 1 || maxScorePlayer.length === 1) {
-        showScore = `${maxScorePlayer[0]}:${maxScore}`;
-        console.log( showScore);
-    } else {
-        showScore = `Exception: ${error}`;
-        console.log( showScore);
-    }
-
-     // Write data in 'Output.txt' .
-    fs.writeFile('output.txt', JSON.stringify(showScore), (error) => {
-        // In case of a error throw error.
-        if (error) throw error;
-    })
+            fs.writeFileSync('output.txt', `${maxSuitScorePlayers.join(",")}:${maxScore}`);
+                } else {
+                fs.writeFileSync('output.txt', `${maxScorePlayer[0]}:${maxScore}`);
+                }
+            } catch (error) {
+                fs.writeFileSync('output.txt', `Exception:${error.message}`);
+            }
+         
 })
 
 
